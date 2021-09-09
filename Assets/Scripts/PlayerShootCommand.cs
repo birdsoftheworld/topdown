0be3f1933981eb73e3314aab32338e9f3400697e
Graham McFarland <graham@sawmac.com>
2021-09-09 15:15:57 -0700

    Functioning Projectiles

--- Assets/Scripts/PlayerShootCommand.cs
+++ Assets/Scripts/PlayerShootCommand.cs
@@ -4,45 +4,24 @@ using UnityEngine;
 
 public class PlayerShootCommand : MonoBehaviour
 {
-    public Collider2D bullet;
+    public Rigidbody2D bullet;
     public float bulletSpeed;
 
+    public Transform bulletOrigin;
+    public GameObject bulletPrefab;
 
-
-
-    void Fire()
-    {
-        // convert mouse position into world coordinates
-        //    Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // get direction you want to point at
-        //Vector2 direction = (mouseScreenPosition - (Vector2) transform.position).normalized;
-
-        // set vector of transform directly
-        // transform.up = direction;
-
-        //Vector3 mouse_pos = Input.mousePosition;
-
-        Collider2D bulletClone = (Collider2D)Instantiate(bullet, transform.position, transform.rotation);
-        //bulletClone.transform.LookAt(Input.mousePosition);
-
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 0;
-
-        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-        mousePos.x = mousePos.x - objectPos.x;
-        mousePos.y = mousePos.y - objectPos.y;
-
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        bulletClone.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
-        //bulletClone.velocity = transform.forward * bulletSpeed;
-    }
-
-    void Update()
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            Fire();
-    }
+        {
+            Vector2 position = bulletOrigin.position;
+            GameObject clone = Instantiate(bulletPrefab, position, bulletOrigin.rotation);
+            clone.gameObject.SetActive(true);
 
+            //clone.gameObject.GetComponent<Projectile>;
+
+            //    clone.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletSpeed, 0), ForceMode2D.Force);
+
+        }
+    }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
 
-    public float speed;
     public float damage;
 
+    public float bulletSpeed;
+    private Rigidbody2D rb2D;
+    private Vector2 destination;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 destinationN = destination.normalized;
+
+        float angle = Mathf.Atan2(destinationN.y, destinationN.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = new Quaternion();
+        rotation.eulerAngles = new Vector3(0, 0, angle + 90);
+
+        transform.rotation = rotation;
+
+        rb2D.AddForce(transform.up * bulletSpeed * -1f);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /*void FixedUpdate()
     {
-
-    //    bulletClone.transform.Translate(mousePos);
-
-
-    }
+        //rb2D.AddForce((transform.up / -10f) * bulletSpeed, ForceMode2D.Impulse);
+        //for constant acceleration
+    }*/
 }
