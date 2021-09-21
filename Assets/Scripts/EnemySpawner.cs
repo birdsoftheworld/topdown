bb9bcc983c50ec2d44186ddce533910699089e1e
Graham McFarland <graham@sawmac.com>
2021-09-21 15:28:23 -0700

    9/21/21 day end. Next: let EC instantiate in rooms on start

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public GameObject healthPrefab;
+
+    public Transform origin;
+
+    public GameObject player;
+
+    public GameObject canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 position = origin.position;
+
+        Spawn(position, origin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn(Vector3 position, Transform rotation)
+    {
+        GameObject clone = Instantiate(enemyPrefab, position, origin.rotation);
+        clone.GetComponent<EnemyFacingBasic>().player = player;
+
+        GameObject cloneHealth = Instantiate(healthPrefab, position, origin.rotation);
+        cloneHealth.transform.SetParent(canvas.transform, false);
+
+
+        clone.GetComponent<HealthTest>().healthBar = cloneHealth.GetComponent<HealthBar>();
+
+        cloneHealth.GetComponent<UIFollow>().monsterPosition = clone.GetComponent<Transform>();
+
+    }
+}
